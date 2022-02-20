81769123a9882cc0f173df9821cba1ace985a8b4
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-02-20 15:21:51 +0700

    feature: sort, search, pagination

--- src/api/products.js
+++ src/api/products.js
@@ -9,3 +9,8 @@ export const get = (id) => {
     const url = `/products/${id}`;
     return instance.get(url);
 };
+
+export const getFilter = (param) => {
+    const url = `/products${param}`;
+    return instance.get(url);
+};
--- src/page/product/Products.js
+++ src/page/product/Products.js
@@ -1,8 +1,22 @@
-import { getAll } from "../../api/products";
+import { getFilter } from "../../api/products";
+import instance from "../../api/config";
+
+const searchParam = new URL(window.location).searchParams;
 
 const Products = {
-    async render() {
-        const { data } = await getAll();
+    async render(cate) {
+
+        const resCate = await instance.get("http://localhost:3001/cateProducts");
+        const dataCate = await resCate.data;
+        const currentCateId = dataCate.find(e => e.name.toLowerCase() == cate).id;
+
+        const searchVal = searchParam.get("q");
+
+        const filterParam = location.search;
+
+        const { data } = await getFilter(`${filterParam}&cateProductId=${currentCateId}`);
+        const dataProducts = data;
+
         const renderStar = (num) => {
             let listStar = "";
             for (let i = 0; i < num; i++) {
@@ -12,10 +26,10 @@ const Products = {
                 </svg>
                 `;
             }
-            console.log(listStar);
             return listStar;
         };
-        const listItem = data.map(e => /*html*/`
+
+        const listItem = dataProducts.map(e => /*html*/`
             <div class="w-1/5 flex flex-col relative">
                 <div class="relative aspect-square rounded-xl overflow-hidden shadow-[0_0_5px_1px_rgba(0,0,0,0.1)]">
                     <img src="${e.img}" class="w-full h-full object-cover">
@@ -26,13 +40,90 @@ const Products = {
                 <div class="absolute px-1 py-1 rounded bg-blend-darken flex flex-col justify-between gap-y-1 bg-indigo-50">${renderStar(e.rate)}</div>
             </div>
         `).join("");
+
+        const listCate = dataCate.map(e => `
+            <a href="/products/${e.name.toLowerCase()}?_page=1&_limit=20" class="py-3 px-3 rounded-[50px] ${e.id == currentCateId ? "bg-red-500 text-white" : "bg-white text-red-500 border-2 border-red-500"}  font-bold text-base text-center">${e.name}</a>
+        `).join("");
+
         return /*html*/`
-            <div class="w-full relative">
-                <div class="max-w-6xl py-8 p-x8 flex justify-between flex-wrap gap-y-5 gap-x-3 mx-auto">
-                    ${listItem}
-                </div>
+            <div class="w-full relative min-h-screen">
+                <div class="max-w-7xl flex mx-auto py-10">
+                    <div class="w-60 flex flex-col px-5 py-5 gap-y-5 sticky top-20">
+                        <div class="flex flex-col gap-y-3">
+                            <h2 class="text-center font-bold text-gray-700 text-2xl pb-3 border-b border-gray-400">Menu</h2>
+                            ${listCate}
+                        </div>
+                        <div class="flex flex-col gap-y-3">
+                            <h2 class="text-center font-bold text-gray-700 text-2xl pb-3 border-b border-gray-400">Sort</h2>
+                            <div class="flex flex-col gap-2">
+                                <p class="text-base font-bold text-gray-900">Sort by Price</p>
+                                <a href="/products/${cate}?${searchVal ? `q=${searchVal}&` : ""}_page=1&_limit=20&_sort=price&_order=asc">Low to High</a>
+                                <a href = "/products/${cate}?${searchVal ? `q=${searchVal}&` : ""}_page=1&_limit=20&_sort=price&_order=desc" > High to Low</a>
+                            </div >
+                        <div class="flex flex-col gap-2">
+                            <p class="text-base font-bold text-gray-900">Sort by Name</p>
+                            <a href="/products/${cate}?${searchVal ? `q=${searchVal}&` : ""}_page=1&_limit=20&_sort=name&_order=asc">A to Z</a>
+                            <a href="/products/${cate}?${searchVal ? `q=${searchVal}&` : ""}_page=1&_limit=20&_sort=name&_order=desc">Z to A</a>
+                        </div>
+                        </div >
+                    </div >
+    <div class="w-[calc(100%_-_240px)] px-5 flex flex-col mx-auto gap-y-5">
+        <form class="flex items-center relative w-96">
+            <input type="text" id="search" placeholder="Search" class="py-2 px-5 w-96 rounded-[50px] border border-red-500 outline-none">
+                <button data-cate="${cate}" id="btn-search" class="absolute right-3 bg-red-500 rounded-[50px] w-7 h-7 flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="white">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+        </form>
+        <div class="flex flex-col gap-y-3 items-center">
+            <div class="w-full flex justify-between gap-y-5 gap-x-3 flex-wrap">
+                ${listItem}
             </div>
-        `;
+            <div class="flex gap-x-3">
+                <button data-cate="${cate}" class="btn-prev w-10 h-6 text-sm text-center leading-6 rounded bg-red-500 text-white">Prev</button>
+                <div class="show-page w-6 h-6 rounded bg-gray-300 text-center text-sm"></div>
+                <button data-cate="${cate}" class="btn-next w-10 h-6 text-sm text-center leading-6 rounded bg-red-500 text-white">Next</button>
+            </div>
+        </div>
+    </div>
+                </div >
+            </div >
+    `;
+    },
+    afterRender() {
+
+        const btnSearch = document.querySelector("#btn-search");
+        const currentPage = Number(searchParam.get("_page"));
+        const searchVal = searchParam.get("q");
+        const orderType = searchParam.get("_order");
+        const sortBy = searchParam.get("_sort");
+
+        btnSearch.onclick = (e) => {
+            e.preventDefault();
+            const cate = btnSearch.dataset.cate;
+            const searchValue = document.querySelector("#search").value.trim();
+            if (searchValue != "") {
+                location.href = `/products/${cate}?q=${searchValue}&_page=${currentPage}&_limit=20`;
+            }
+        };
+
+        document.querySelector(".show-page").innerHTML = currentPage;
+
+        const btnPrev = document.querySelector(".btn-prev");
+        const btnNext = document.querySelector(".btn-next");
+
+        btnPrev.onclick = () => {
+            if (currentPage == 1) return;
+            const cate = btnPrev.dataset.cate;
+            location.href = `/products/${cate}?_page=${currentPage - 1}&_limit=20`;
+        };
+
+        btnNext.onclick = () => {
+            const cate = btnPrev.dataset.cate;
+            location.href = `/products/${cate}?_page=${currentPage + 1}&_limit=20`;
+        };
+
     }
 };
 
